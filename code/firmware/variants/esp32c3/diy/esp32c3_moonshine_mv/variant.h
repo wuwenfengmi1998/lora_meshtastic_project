47d581432508c965f55feef5031475e5a608f8ff
吴文峰 <kevin@lmve.net>
2026-03-04 21:58:48 +0800

    up

--- code/firmware/variants/esp32c3/diy/esp32c3_moonshine_mv/variant.h
+++ code/firmware/variants/esp32c3/diy/esp32c3_moonshine_mv/variant.h
@@ -23,12 +23,12 @@
 #define GPS_POWER_TOGGLE 1
 #define PIN_GPS_EN 12
 
-#undef  BATTERY_PIN
-#define BATTERY_PIN            2
-#define ADC_CHANNEL            ADC1_GPIO1_CHANNEL
+// #undef  BATTERY_PIN
+// #define BATTERY_PIN            2
+// #define ADC_CHANNEL            ADC1_GPIO1_CHANNEL
 
-#undef  ADC_MULTIPLIER
-#define ADC_MULTIPLIER         2.0f
+// #undef  ADC_MULTIPLIER
+// #define ADC_MULTIPLIER         2.0f
 
 // #define HAS_NEOPIXEL 1
 // #define NEOPIXEL_DATA 13
